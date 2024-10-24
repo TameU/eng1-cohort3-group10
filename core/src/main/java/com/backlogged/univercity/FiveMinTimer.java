250e939abca1730070855992cf8faea77c279ee0
nk_t14 <aclatm.20@gmail.com>
2024-10-24 18:16:37 +0100

    Basic count down timer implementation. Potential changes:     - Should probably be static?     - We should probably have a deltaTime member in the main game class

---
+++ core/src/main/java/com/backlogged/univercity/FiveMinTimer.java
@@ -0,0 +1,29 @@
+package com.backlogged.univercity;
+import com.badlogic.gdx.Gdx;
+public class FiveMinTimer {
+    private float currentTimeRemaining = 60 * 5;
+    boolean hasStopped = true;
+    public FiveMinTimer() {
+        
+    } 
+    public void startTime() { 
+        hasStopped = false;
+    }  
+    public void stopTime() { 
+        hasStopped = true;
+    }
+    public void update() { 
+        if (hasStopped) return; 
+        float deltaTime = Gdx.graphics.getDeltaTime();
+        currentTimeRemaining = currentTimeRemaining - deltaTime < 0.f ? 0.f : currentTimeRemaining - deltaTime;
+    } 
+    public int getCurrentTime() { 
+        return (int) currentTimeRemaining;
+    } 
+    public String toString() { 
+        int time = getCurrentTime();
+        int seconds = time % 60;
+        int minutes = time / 60; 
+        return String.format("%02d:%02d", minutes, seconds);
+    }
+}
