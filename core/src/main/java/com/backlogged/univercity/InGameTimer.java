c35514da0a1df65a96c40b84a7427118c0441092
nk_t14 <aclatm.20@gmail.com>
2024-10-25 01:20:12 +0100

    I dont know what happened but this should be the right file now

--- core/src/main/java/com/backlogged/univercity/InGameTimer.java
+++ core/src/main/java/com/backlogged/univercity/InGameTimer.java
@@ -1,10 +1,13 @@
 package com.backlogged.univercity;
 public class InGameTimer {
     private float currentTimeRemaining;
-    boolean hasStopped = true;
+    private int timeInMinutes;
+    private boolean hasStopped = true;
     public InGameTimer(int timeInMinutes) {
         /**TODO: Support time beyond 60mins maybe?*/
-        currentTimeRemaining = timeInMinutes * 60;
+        
+        currentTimeRemaining = (float)timeInMinutes * 60.f;
+        this.timeInMinutes = timeInMinutes;
     } 
     public void startTime() { 
         hasStopped = false;
@@ -14,17 +17,15 @@ public class InGameTimer {
     } 
     public void resetTime() { 
         stopTime();
-        currentTimeRemaining = 60.f * 5.f; 
+        currentTimeRemaining = (float)timeInMinutes * 60.f;
     }
     public void update(float delta) { 
         if (hasStopped) return;
-        currentTimeRemaining = currentTimeRemaining - delta < 0.f ? 0.f : currentTimeRemaining - delta;
+        currentTimeRemaining = currentTimeRemaining - delta <= 0.f ? 0.f : currentTimeRemaining - delta;
     } 
-    public int getCurrentTime() { 
-        return (int) currentTimeRemaining;
-    } 
+    
     public String toString() { 
-        int time = getCurrentTime();
+        int time = (int) currentTimeRemaining;
         int seconds = time % 60;
         int minutes = time / 60; 
         return String.format("%02d:%02d", minutes, seconds);
