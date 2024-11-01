5cdc9c97ee9a90a75a26a08346e6b010a29eb189
tim4ik <ttopala@outlook.com>
2024-11-01 19:27:07 +0000

    Removed unused imports

--- core/src/main/java/com/backlogged/univercity/GameOverScreen.java
+++ core/src/main/java/com/backlogged/univercity/GameOverScreen.java
@@ -3,7 +3,6 @@ package com.backlogged.univercity;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.scenes.scene2d.Event;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
