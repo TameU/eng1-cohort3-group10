76ad5a493054321874402b1fe5b36d1b9f1da41b
tim4ik <ttopala@outlook.com>
2024-11-01 19:20:34 +0000

    Removed unused imports

--- core/src/main/java/com/backlogged/univercity/TitleScreen.java
+++ core/src/main/java/com/backlogged/univercity/TitleScreen.java
@@ -2,15 +2,9 @@ package com.backlogged.univercity;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TmxMapLoader;
-import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
-import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
