36346dae9bfb248b0a75151850118233dba5bd67
TameU <tamerlanurazbayev@gmail.com>
2024-11-04 15:09:45 +0000

    Added icons to screen (bugged)

--- core/src/main/java/com/backlogged/univercity/MapScreen.java
+++ core/src/main/java/com/backlogged/univercity/MapScreen.java
@@ -26,6 +26,7 @@ import com.badlogic.gdx.utils.viewport.ScreenViewport;
  */
 public class MapScreen implements Screen {
 
+<<<<<<< HEAD
   private Game game;
   private TiledMap map;
   private float unitScale;
@@ -42,6 +43,29 @@ public class MapScreen implements Screen {
   private boolean dragging;
   private float oldMouseX;
   private float oldMouseY;
+=======
+    private Game game;
+    private TiledMap map;
+    private float unitScale;
+    private OrthogonalTiledMapRenderer renderer;
+    private OrthographicCamera camera;
+    private Skin skin;
+    private Stage stage;
+    private Table table;
+    private Label timerLabel;
+    private Button pauseButton;
+    private Button settingsButton;
+    private InGameTimer timer;
+    private boolean mouseDown;
+    private boolean dragging;
+    private float oldMouseX, oldMouseY;
+    private float sensitivity = 0.025f;
+    // Buildings
+    private Button bed;
+    private Button book;
+    private Button food;
+    private Button football;
+>>>>>>> a51e9ec (Added icons to screen (bugged))
 
   /**
    * Sets up the Map screen to an initial state.
@@ -59,6 +83,7 @@ public class MapScreen implements Screen {
     timerLabel = new Label("5:00", skin);
     timerLabel.setAlignment(Align.center);
 
+<<<<<<< HEAD
     pauseButton = new Button(skin, "pause");
     pauseButton.addListener(new ClickListener() {
       public void clicked(InputEvent e, float x, float y) {
@@ -66,6 +91,105 @@ public class MapScreen implements Screen {
           timer.userStartTime();
         } else {
           timer.userStopTime();
+=======
+        pauseButton = new Button(skin, "pause");
+        pauseButton.addListener(new ClickListener() {
+            public void clicked(InputEvent e, float x, float y) {
+                if (timer.isUserStopped())
+                    timer.userStartTime();
+                else
+                    timer.userStopTime();
+            }
+        });
+
+        settingsButton = new Button(skin, "settings");
+        settingsButton.addListener(new ClickListener() {
+            public void clicked(InputEvent e, float x, float y) {
+                timer.userStopTime(); // pause Time while in settings
+                game.setScreen(new SettingsScreen(game, game.getScreen()));
+            }
+        });
+
+        //////////////// BUILDINGS
+
+        bed = new Button(skin, "bed");
+        bed.addListener(new ClickListener() {
+            public void clicked(InputEvent e, float x, float y) {
+                // Deal with clicking later
+            }
+        });
+
+        book = new Button(skin, "book");
+        book.addListener(new ClickListener() {
+            public void clicked(InputEvent e, float x, float y) {
+                // Deal with clicking later
+            }
+        });
+
+        food = new Button(skin, "food");
+        food.addListener(new ClickListener() {
+            public void clicked(InputEvent e, float x, float y) {
+                // Deal with clicking later
+            }
+        });
+
+        football = new Button(skin, "football");
+        football.addListener(new ClickListener() {
+            public void clicked(InputEvent e, float x, float y) {
+                // Deal with clicking later
+            }
+        });
+
+        timerLabel.addListener(new ClickListener() {
+            public void clicked(InputEvent e, float x, float y) {
+                game.setScreen(new GameOverScreen(game));
+            }
+        });
+
+        table = new Table(skin);
+        table.setFillParent(true);
+        table.setDebug(true);
+        table.add(timerLabel).expand().top();
+        table.add(pauseButton).top().right();
+        table.add(settingsButton).top();// .left();
+        // Buildings
+        table.add(bed).center().left();
+        table.add(book).center().left();
+        table.add(food).center().left();
+        table.add(football).center().left();
+
+        stage.addActor(table);
+
+        map = new TmxMapLoader().load("desert.tmx");
+        unitScale = 1 / 32f;
+        renderer = new OrthogonalTiledMapRenderer(map, unitScale);
+        camera = new OrthographicCamera();
+        float width = Gdx.graphics.getWidth();
+        float height = Gdx.graphics.getHeight();
+        camera.setToOrtho(false, width * unitScale, (width * unitScale) * (height / width));
+        timer.resetTime();
+        timer.userStartTime();
+    }
+
+    @Override
+    public void show() {
+        Gdx.input.setInputProcessor(stage);
+        timer.initialiseTimerValues();
+    }
+
+    @Override
+    public void render(float delta) {
+        handleInput();
+        camera.update();
+        renderer.setView(camera);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        renderer.render();
+        float timeLeft = timer.updateTime(delta);
+        float elapsedTime = timer.timeElapsed(delta);
+
+        if (elapsedTime > Constants.ONE_MONTH) {
+            timer.updateTimerValues();
+>>>>>>> a51e9ec (Added icons to screen (bugged))
         }
       }
     });
