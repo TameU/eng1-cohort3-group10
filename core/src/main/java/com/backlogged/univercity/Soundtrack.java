16b9e70c0cdfc4d570a6c6e6d048aa3e1dab2568
tim4ik <ttopala@outlook.com>
2024-11-07 17:37:36 +0000

    docs for the soundtrack class

--- core/src/main/java/com/backlogged/univercity/Soundtrack.java
+++ core/src/main/java/com/backlogged/univercity/Soundtrack.java
@@ -4,18 +4,30 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
 
 public final class Soundtrack {
+    /**
+    * This class is a singleton that manages and plays the game's soundtrack.
+    * The soundtrack is set to loop continuously when played.
+    */
+
     private Soundtrack() {
         // restricts instantiation of class
     }
 
     private static final Music soundtrack = Gdx.audio.newMusic(Gdx.files.internal(Constants.SOUNDTRACK_PATH));
     private static final float volume = 1f;
+
+    /**
+     * Starts or resumes the soundtrack. Updates volume.
+     */
     public static void play() {
         soundtrack.setVolume(volume);
         soundtrack.setLooping(true);
         soundtrack.play();
     }
 
+    /**
+     * Pauses the soundtrack.
+     */
     public static void pause() {
         soundtrack.pause();
     }
