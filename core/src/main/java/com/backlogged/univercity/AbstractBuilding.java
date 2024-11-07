f729aefddb8ef3becada29a6d7405b76092bc017
nk_t14 <aclatm.20@gmail.com>
2024-11-07 15:40:49 +0000

    feat: can place buildings

---
+++ core/src/main/java/com/backlogged/univercity/AbstractBuilding.java
@@ -0,0 +1,35 @@
+package com.backlogged.univercity;
+
+import java.util.ArrayList;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+public abstract class AbstractBuilding {
+    private BuildingInfo buildingInfo; 
+    private Coord mapPos; 
+    public AbstractBuilding(BuildingInfo buildingInfo) { 
+        this.buildingInfo = buildingInfo;
+    } 
+        public final String[] getType() {
+        return buildingInfo.type;
+    } 
+    public final Sprite getSprite() { 
+        return buildingInfo.buildingSprite;
+    } 
+    public final ArrayList<Coord> getTileCoverageOffsets() { 
+        return buildingInfo.tileCoverageOffsets;
+    } 
+    public final String getInfo() { 
+        return buildingInfo.info;
+    }  
+    public final void setMapPosition(Coord mapPos) { 
+        this.mapPos = mapPos;
+    } 
+    public final Coord getMapPos() { 
+        return mapPos;
+    } 
+    public final void draw(SpriteBatch batch) { 
+        buildingInfo.buildingSprite.setPosition(this.mapPos.getRow(), this.mapPos.getColumn());
+        buildingInfo.buildingSprite.draw(batch); 
+        buildingInfo.buildingSprite.setPosition(0,0);
+    }
+}
---
+++ core/src/main/java/com/backlogged/univercity/LectureHall.java
@@ -0,0 +1,7 @@
+package com.backlogged.univercity;
+
+public class LectureHall extends AbstractBuilding {
+    public LectureHall(BuildingInfo buildingInfo) { 
+        super(buildingInfo);
+    }
+}
