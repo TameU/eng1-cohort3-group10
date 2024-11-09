f93221f5f8666ebb4070ab5a5b64eff3e2d20fdb
nk_t14 <aclatm.20@gmail.com>
2024-11-09 20:09:31 +0000

    update

--- core/src/main/java/com/backlogged/univercity/IBuildingRenderer.java
+++ core/src/main/java/com/backlogged/univercity/IBuildingRenderer.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 public interface IBuildingRenderer {
     public void renderBuildings(Collection<AbstractBuilding> placedBuildings, OrthographicCamera camera); 
-    public void renderPlacementSquares(int row, int column, OrthographicCamera camera,
+    public void renderPlacementSquares(boolean canBePlacedAtLocation, int row, int column, OrthographicCamera camera,
     AbstractBuilding building); 
     public TextureAtlas getAtlas();
 }
