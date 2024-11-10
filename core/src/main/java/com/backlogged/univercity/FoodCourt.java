a86ab7706fbd6324550c021d1a81ea1c2f664ed0
nk_t14 <aclatm.20@gmail.com>
2024-11-10 00:27:28 +0000

    addition javadocs

--- core/src/main/java/com/backlogged/univercity/FoodCourt.java
+++ core/src/main/java/com/backlogged/univercity/FoodCourt.java
@@ -1,5 +1,7 @@
 package com.backlogged.univercity;
-
+/**
+ * This class represents a basic food court.
+ */
 public class FoodCourt extends AbstractBuilding {
     public FoodCourt(BuildingInfo buildingInfo) { 
         super(buildingInfo);
--- core/src/main/java/com/backlogged/univercity/LectureHall.java
+++ core/src/main/java/com/backlogged/univercity/LectureHall.java
@@ -1,5 +1,7 @@
 package com.backlogged.univercity;
-
+/**
+ * This class represents a basic Lecture Hall.
+ */
 public class LectureHall extends AbstractBuilding {
     public LectureHall(BuildingInfo buildingInfo) { 
         super(buildingInfo);
